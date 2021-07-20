d6ed6795a324bc87f300439ce9a1dc8e2c6206eb
gogijoa <68880872+gogijoa@users.noreply.github.com>
2021-07-20 15:24:46 +0900

    Add files via upload

---
+++ Double_for_loops.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    for j in range(10):
+        print("j:", j, sep=" ", end = " ")
+    print("i:", i, "\\n", sep="")
+
+
+
+#times table 11~19 
+for i in range(11,19):
+    for j in range(1,10):
+        print(f"{i} x {j} = {i*j}")
